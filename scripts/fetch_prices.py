e742ed77a4c1b4a196f49cf69ad3b549e29aa84e
koszyk99 <krzysztof_koszyk@interia.eu>
2026-03-31 17:11:12 +0200

    feat: adding communication beetwen python and psql

--- scripts/fetch_prices.py
+++ scripts/fetch_prices.py
@@ -4,18 +4,19 @@ import psycopg2
 from dotenv import load_dotenv
 import os
 
-response = requests.get('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
+# Choose which coin we want to track
+coins = ['bitcoin', 'ethereum', 'solana', 'cardano']
+ids_param = ",".join(coins)
+
+# Data download
+response = requests.get(f'https://api.coingecko.com/api/v3/simple/price?ids={ids_param}&vs_currencies=usd')
 print(response.url)
 print(response.status_code)
 
 # Convert a raw response from the internet into a Python dictionary
 data = response.json()
 
-# Fetch prices
-price = data['bitcoin']['usd']
-print(f"Success! Bitcoin price: {price} USD")
-
-# read .env file
+# Read .env file
 load_dotenv()
 
 db_user = os.getenv('DB_USER')
@@ -32,5 +33,25 @@ try:
         port='5432'
     )
     print(f"Connected to database: {db_name}")
+
+    # Create cursor
+    cur = conn.cursor()
+
+    for coin_id, coin_info in data.items():
+        price = coin_info['usd']
+        symbol = coin_id.upper()
+
+        # Execute method for sql query
+        sql_query = "INSERT INTO raw_crypto_prices (symbol, price) VALUES (%s, %s)"
+        cur.execute(sql_query, (symbol, price))
+        print(f"I'm putting in: {symbol} - {price} USD")
+
+    # "Save"
+    conn.commit()
+
+    # End work and close cursor
+    cur.close()
+    conn.close()
+
 except Exception as e:
     print(f"Connection error: {e}")
